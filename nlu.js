4a0c24fa6d53e8da5ba456eb64737cff00d99fa1
Kikumen <kikumen91@gmail.com>
2019-03-01 10:06:47 +0100

    wit.meaning() compara el texto introducido connuestro patron de aprendizaje

--- nlu.js
+++ nlu.js
@@ -1,3 +1,9 @@
 const TelegrafWit = require('telegraf-wit')
 
 const wit = new TelegrafWit('RW6CG24E72IHKLTCCSMLENVBRJDXEDT5')
+
+module.exports=(message)=>{
+    return new Promise((resolve,reject)=>{
+        wit.meaning()
+    })
+}
